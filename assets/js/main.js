b4bcd09c796a6be868130bcac8e8c4a41eba50f8
Diego Orellana <diego.orellana.git@gmail.com>
2023-10-02 15:34:44 -0300

    finished mobile design

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,8 +36,15 @@ window.addEventListener('scroll', shadowHeader)
 
 /*=============== EMAIL JS ===============*/
 
-
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        :scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll',scrollUp)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
