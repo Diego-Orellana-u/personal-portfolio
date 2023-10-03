fd864b7a5e25d7820253cbc5030279f7abc11169
Diego Orellana <diego.orellana.git@gmail.com>
2023-10-02 23:23:23 -0300

    added dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,8 +49,36 @@ window.addEventListener('scroll',scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute('id');
+        sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
 
 /*=============== DARK LIGHT THEME ===============*/ 
+
+const themeButton = document.getElementById('theme-button')
+
+const changeTheme = () => {
+    document.body.classList.toggle('dark-theme')
+}
+
+themeButton.addEventListener('click',changeTheme )
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
